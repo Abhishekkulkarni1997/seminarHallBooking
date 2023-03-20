0c719cb342ff77656b4a697e908aba3393a1e3c9
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-03-20 16:32:12 +0530

    added admin schema

--- Model/Admin.schema.js
+++ Model/Admin.schema.js
@@ -1,1 +1,47 @@
 // create admin schema
+import mongoose from "mongoose";
+import AuthRoles from "../utils/AuthRoles";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import config from "../config";
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      maxLength: [15, "the username should be of less that 15 characthers "],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minLength: [6, "password must be atleast 6 Characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.DEPARTMENT,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified(this.password)) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export default mongoose.model("Admin", adminSchema);
