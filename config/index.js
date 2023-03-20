a4add50c1410c412ced728a896033220c170fc28
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-03-20 16:32:20 +0530

    added config file

---
+++ config/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+  MONGODB_URL: process.env.MONGODB_URL,
+  PORT: process.env.PORT,
+};
+
+export default config;
